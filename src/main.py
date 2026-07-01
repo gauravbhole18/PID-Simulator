ccafe0a884bf9d62d75da99b726e08150f5abf2c
gauravbhole <bholegaurav183@gmail.com>
2026-07-01 23:04:23 +0530

    added TemperaturePlant skeleton

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from plant import TemperaturePlant
+
+plant = TemperaturePlant()
+
+print(f"Initial temperature: {plant.temperature} °C")
+print(f"Room temperature: {plant.room_temperature} °C")
+print(f"Heating constant: {plant.heating_constant}")
+print(f"Cooling constant: {plant.cooling_constant}")
---
+++ src/plant.py
@@ -0,0 +1,13 @@
+class TemperaturePlant:
+    def __init__(self):
+        self.room_temperature = 20.0
+
+        self.temperature = 20.0  # Initial temperature of the plant
+
+        self.heating_constant = 0.8
+
+        self.cooling_constant = 0.03
+
+    def update(self, heater_power, dt):
+        pass
+        
